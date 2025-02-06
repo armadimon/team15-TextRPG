35580004a97f7c9692ac94e2a16ede87999fe687
Jung Ha Na <73771420+onehana1@users.noreply.github.com>
2025-02-06 20:54:13 +0900

    feat: 적 추가
    - Enemy clsss, EnemyManager 추가
    - 적매니저는 싱글톤 패턴 사용

---
+++ EnemyManager.cs
@@ -0,0 +1,42 @@
+﻿using _15TextRPG.Source.State;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15TextRPG
+{
+    public class EnemyManager
+    {
+        private static EnemyManager? instance;
+        private List<Enemy> enemyList; // 적 목록
+
+        private EnemyManager()
+        {
+            enemyList = new List<Enemy>()
+            {
+                new Enemy("옴닉a", 1, "옴닉a이다.", 5, 2, 30, new List<Item>()),
+                new Enemy("옴닉b", 1, "옴닉b이다.", 5, 2, 30, new List<Item>()),
+                new Enemy("옴닉c", 1, "옴닉c이다.", 5, 2, 30, new List<Item>()),
+            };
+        }
+
+        public static EnemyManager Instance // 싱글톤
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new EnemyManager();
+                return instance;
+            }
+        }
+
+        public Enemy GetRandomEnemy()
+        {
+            Random random = new Random();
+            int index = random.Next(enemyList.Count);
+            return new Enemy(enemyList[index]);
+        }
+    }
+}
