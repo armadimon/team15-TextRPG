761d36959a370699a4cadffa8f169f04eb1ffc82
Jung Ha Na <73771420+onehana1@users.noreply.github.com>
2025-02-06 20:41:28 +0900

    refactor: Program.cs에서 각 클래스를 개별 파일로 이동
    - Program.cs에서 각 클래스를 개별 파일로 이동
    - Player, GameManager 이동

---
+++ GameManager.cs
@@ -0,0 +1,44 @@
+﻿using _15TextRPG.Source.State;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15TextRPG
+{
+    public class GameManager
+    {
+        private IGameState currentState;
+        private bool isRunning = true;
+        public Player Player { get; private set; }
+
+        public GameManager()
+        {
+            Player = new Player("Default");
+            currentState = new MainMenuState();
+        }
+
+        public void Run()
+        {
+            while (isRunning)
+            {
+                currentState.DisplayMenu(this);
+                Console.Write("\n원하시는 행동을 입력해주세요. >> ");
+                string input = Console.ReadLine() ?? "";
+                currentState.HandleInput(this, input);
+            }
+        }
+
+        public void ChangeState(IGameState newState)
+        {
+            currentState = newState;
+        }
+
+        public void QuitGame()
+        {
+            Console.WriteLine("게임을 종료합니다.");
+            isRunning = false;
+        }
+    }
+}
