0886f60705c4287ad00ffdbf42959661ffcd94c2
cuervo314 <gkswjddn30635@naver.com>
2025-02-11 14:50:19 +0900

    chore: 몬스터 관련 용어 및 위치 정리

--- Source/IMonster.cs
+++ Source/IMonster.cs
@@ -11,6 +11,7 @@ namespace _15TextRPG.Source
         string Type { get; set; }
         string MonsterName { get; set; }
         int Level { get; set; }
+        double MaxHealth { get; set; }
         double Health { get; set; }
         string SkillName { get; set; }
         int AttackDamage { get; set; }
@@ -18,15 +19,17 @@ namespace _15TextRPG.Source
         double ArmorRisistence { get; set; }
         double SkillRisistence { get; set; }
 
-        void Attak(Player player)
+        void Attack(Player player)
         {
-            Console.WriteLine($"{MonsterName}(이/가) 공격했다.");
+            Console.WriteLine($"{MonsterName}(이/가) 공격합니다.");
+            Console.ReadLine();
             player.Health -= AttackDamage;
         }
 
         void UseSkill(Player player)
         {
-            Console.WriteLine($"{MonsterName}(이/가) {SkillName}(을/를) 사용했다.");
+            Console.WriteLine($"{MonsterName}(이/가) {SkillName}(을/를) 사용합니다.");
+            Console.ReadLine();
             player.Health -= SkillDamage;
         }
 
@@ -37,24 +40,27 @@ namespace _15TextRPG.Source
         public string Type { get; set; } = "robo";
         public string MonsterName { get; set; } = "경비로봇";
         public int Level { get; set; } = 1;
-        public double Health { get; set; } = 100;
+        public double MaxHealth { get; set; } = 50;
+        public double Health { get; set; } = 50;
         public string SkillName { get; set; } = "몽둥이질";
-        public int AttackDamage { get; set; } = 2;
-        public int SkillDamage { get; set; } = 5;
+        public int AttackDamage { get; set; } = 5;
+        public int SkillDamage { get; set; } = 7;
         public double ArmorRisistence { get; set; } = 0;
         public double SkillRisistence { get; set; } = 0;
 
-        public void Attak(Player player)
-        {
-            Console.WriteLine($"{MonsterName}(이/가) 공격했다.");
-            player.Health -= AttackDamage;
-        }
+        //void Attack(Player player)
+        //{
+        //    Console.WriteLine($"{MonsterName}(이/가) 공격합니다.");
+        //    Console.ReadLine();
+        //    player.Health -= AttackDamage;
+        //}
 
-        public void UseSkill(Player player)
-        {
-            Console.WriteLine($"{MonsterName}(이/가) {SkillName}(을/를) 사용했다.");
-            player.Health -= SkillDamage;
-        }
+        //void UseSkill(Player player)
+        //{
+        //    Console.WriteLine($"{MonsterName}(이/가) {SkillName}(을/를) 사용합니다.");
+        //    Console.ReadLine();
+        //    player.Health -= SkillDamage;
+        //}
     }
 
     public class Cybo : IMonster
@@ -62,24 +68,13 @@ namespace _15TextRPG.Source
         public string Type { get; set; } = "cybo";
         public string MonsterName { get; set; } = "경비팀장";
         public int Level { get; set; } = 2;
-        public double Health { get; set; } = 125;
+        public double MaxHealth { get; set; } = 75;
+        public double Health { get; set; } = 75;
         public string SkillName { get; set; } = "몽둥이질";
-        public int AttackDamage { get; set; } = 3;
-        public int SkillDamage { get; set; } = 5;
+        public int AttackDamage { get; set; } = 7;
+        public int SkillDamage { get; set; } = 10;
         public double ArmorRisistence { get; set; } = 1;
         public double SkillRisistence { get; set; } = 1;
-
-        public void Attak(Player player)
-        {
-            Console.WriteLine($"{MonsterName}(이/가) 공격했다.");
-            player.Health -= AttackDamage;
-        }
-
-        public void UseSkill(Player player)
-        {
-            Console.WriteLine($"{MonsterName}(이/가) {SkillName}(을/를) 사용했다.");
-            player.Health -= SkillDamage;
-        }
     }
 
     public class Human : IMonster
@@ -87,23 +82,12 @@ namespace _15TextRPG.Source
         public string Type { get; set; } = "human";
         public string MonsterName { get; set; } = "경찰";
         public int Level { get; set; } = 3;
-        public double Health { get; set; } = 150;
+        public double MaxHealth { get; set; } = 100;
+        public double Health { get; set; } = 100;
         public string SkillName { get; set; } = "스턴건";
-        public int AttackDamage { get; set; } = 3;
-        public int SkillDamage { get; set; } = 7;
+        public int AttackDamage { get; set; } = 10;
+        public int SkillDamage { get; set; } = 12;
         public double ArmorRisistence { get; set; } = 2;
         public double SkillRisistence { get; set; } = 2;
-
-        public void Attak(Player player)
-        {
-            Console.WriteLine($"{MonsterName}(이/가) 공격했다.");
-            player.Health -= AttackDamage;
-        }
-
-        public void UseSkill(Player player)
-        {
-            Console.WriteLine($"{MonsterName}(이/가) {SkillName}(을/를) 사용했다.");
-            player.Health -= SkillDamage;
-        }
     }
 }
