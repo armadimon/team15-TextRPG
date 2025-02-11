6b496c0a3441907df2d3dc7baf56a53c8a3168f2
cuervo314 <gkswjddn30635@naver.com>
2025-02-11 19:34:05 +0900

    feat: 플레이어 스탯, 클래스 추가

--- Source/Player.cs
+++ Source/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using _15TextRPG.Source.Combat;
@@ -15,6 +16,10 @@ namespace _15TextRPG.Source
         public double SkillDamage { get; set; }
         public int DefensePoint { get; set; }
         public int SkillDefensePoint { get; set; }
+        public int Str { get; set; }
+        public int Dex { get; set; }
+        public int Critical { get; set; }
+        public int Dodge { get; set; }
         public int MaxHP { get; set; }
         public int Health { get; set; }
         public int MaxMP { get; set; }
@@ -29,23 +34,74 @@ namespace _15TextRPG.Source
 
         public Inventory Inventory { get; set; }
 
-        public Player(string name)
+        public Player(string name, int description) // 힘캐
         {
             Name = name;
             Level = 1;
             Exp = 0;
             MaxExp = 5;
-            Description = "용병";
-            AttackDamage = 50;
-            SkillDamage = 50;
-            DefensePoint = 5;
+            Description = "노마드";
+            Str = 3;
+            Dex = 1;
+            Critical = 20 + Dex;
+            Dodge = 15 + Dex;
+            AttackDamage = 40 + Str;
+            SkillDamage = 20 + Dex;
+            DefensePoint = 5 + Str;
+            SkillDefensePoint = 5 + Str;
             MaxHP = 100;
             Health = MaxHP;
-            MaxMP = 50;
+            MaxMP = 25;
             MP = MaxMP;
             Gold = 1500;
             Inventory = new(this);
         }
+
+        //public Player(string name, int description) // 민첩캐
+        //{
+        //    Name = name;
+        //    Level = 1;
+        //    Exp = 0;
+        //    MaxExp = 5;
+        //    Description = "부랑아";
+        //    Str = 1;
+        //    Dex = 3;
+        //    Critical = 25 + Dex;
+        //    Dodge = 20 + Dex;
+        //    AttackDamage = 20 + Str;
+        //    SkillDamage = 40 + Dex;
+        //    DefensePoint = 5 + Str;
+        //    SkillDefensePoint = 5 + Str;
+        //    MaxHP = 100;
+        //    Health = MaxHP;
+        //    MaxMP = 50;
+        //    MP = MaxMP;
+        //    Gold = 1500;
+        //    Inventory = new(this);
+        //}
+
+        //public Player(string name, int description) / 지능캐
+        //{
+        //    Name = name;
+        //    Level = 1;
+        //    Exp = 0;
+        //    MaxExp = 5;
+        //    Description = "기업";
+        //    Str = 1;
+        //    Dex = 1;
+        //    Critical = 20 + Dex;
+        //    Dodge = 15 + Dex;
+        //    AttackDamage = 20 + Str;
+        //    SkillDamage = 20 + Str;
+        //    DefensePoint = 2 + Str;
+        //    SkillDefensePoint = 2 + Str;
+        //    MaxHP = 100;
+        //    Health = MaxHP;
+        //    MaxMP = 50;
+        //    MP = MaxMP;
+        //    Gold = 1500;
+        //    Inventory = new (this);
+        //}
 
         public void ShowStatus()
         {
@@ -72,9 +128,20 @@ namespace _15TextRPG.Source
         {
             Random random = new Random();
             int j = random.Next(90, 111);
+            int k = random.Next(0, 100);
             Console.WriteLine($"{gameManager.BattleManager.monsters[i].MonsterName}(을/를) 공격합니다");
-            Console.ReadLine();
-            gameManager.BattleManager.monsters[i].Health -= Math.Round(gameManager.GameData.Player.AttackDamage * j / 100) + gameManager.BattleManager.monsters[i].ArmorRisistence;
+            Thread.Sleep(1500);
+
+            if( k <= gameManager.GameData.Player.Critical)
+            {
+                Console.WriteLine($"{gameManager.BattleManager.monsters[i].MonsterName}에게 치명적인 피해를 입혔습니다.");
+                Thread.Sleep(1500);
+                gameManager.BattleManager.monsters[i].Health -= Math.Round(gameManager.GameData.Player.AttackDamage * j / 100) * 1.5 + gameManager.BattleManager.monsters[i].ArmorRisistence;
+            }
+            else
+            {
+                gameManager.BattleManager.monsters[i].Health -= Math.Round(gameManager.GameData.Player.AttackDamage * j / 100) + gameManager.BattleManager.monsters[i].ArmorRisistence;
+            }            
 
             if (gameManager.BattleManager.monsters[i].Health <= 0)
             {
@@ -92,15 +159,15 @@ namespace _15TextRPG.Source
                 gameManager.BattleManager.ShowMonster(false, 0, 9);
                 Console.WriteLine();
                 Console.WriteLine($"{gameManager.BattleManager.monsters[i].MonsterName}(이/가) 쓰러졌습니다.");
-                Console.ReadLine();
+                Thread.Sleep(1500);
             }
         }
 
         public void UseSkill(GameManager gameManager, int i, ISKill skill, IMonster monster)
         {
             Console.WriteLine($"{gameManager.BattleManager.monsters[i].MonsterName}에게 {skill.SkillName}(을/를) 사용합니다");
-            Console.ReadLine();
-            if(skill.SkillType == monster.Type)
+            Thread.Sleep(1500);
+            if (skill.SkillType == monster.Type)
             {
                 gameManager.BattleManager.monsters[i].Health -= (gameManager.GameData.Player.SkillDamage + skill.BonusDamage) - gameManager.BattleManager.monsters[i].SkillRisistence;
             }
@@ -127,7 +194,7 @@ namespace _15TextRPG.Source
                 gameManager.BattleManager.ShowMonster(false, 0, 9);
                 Console.WriteLine();
                 Console.WriteLine($"{gameManager.BattleManager.monsters[i].MonsterName}(이/가) 쓰러졌습니다.");
-                Console.ReadLine();
+                Thread.Sleep(1500);
             }
         }
     }
